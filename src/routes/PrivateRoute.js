30c9df8812edeac5462bbd88096b5245fdf10412
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-10 01:58:28 +0600

    Spinner added on page loading

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,19 +1,25 @@
-import React, { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../contexts/UserContext';
+import { Button, Spinner } from "flowbite-react";
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../contexts/UserContext";
 
-const PrivateRoute = ({children}) => {
-    const {user,loading} = useContext(AuthContext)
-    const location = useLocation();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-    if(loading) {
-        return <div>Loading...</div>
-    }
+  if (loading) {
+    return (
+      <Button className="w-2/12 mx-auto my-10">
+        <Spinner aria-label="Spinner button example" />
+        <span className="pl-3">Loading...</span>
+      </Button>
+    );
+  }
 
-    if(user) {
-        return children
-    }
-    return <Navigate to='/login' state= { {from : location} } replace></Navigate>
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
