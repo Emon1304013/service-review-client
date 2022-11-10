4d9ca1e05de68f6a842ba6ee8a22062e7efc403a
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-10 15:51:20 +0600

    Statistics page added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import { useTitle } from '../../hooks/useTitle';
 import HeroSection from './HeroSection';
 import ServiceSection from './ServiceSection/ServiceSection';
 import Timeline from './MyTimeline';
+import Stats from './Stats';
 
 const Home = () => {
     useTitle('Home')
@@ -11,6 +12,7 @@ const Home = () => {
             <HeroSection></HeroSection>
             <ServiceSection></ServiceSection>
             <Timeline></Timeline>
+            <Stats></Stats>
         </div>
     );
 };
